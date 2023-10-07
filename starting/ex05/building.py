8a2becffb46e9082c2819ccb81f9f9924ca004cc
gottie <gottiedev@gmail.com>
2023-10-07 12:44:34 +0200

    ex05 done

--- starting/ex05/building.py
+++ starting/ex05/building.py
@@ -1,11 +1,30 @@
+import sys
 from sys import argv
+import string
 
 def getstr() -> str:
     if len(argv) == 2:
         return argv[1]
-    return input("What is the text to count?\n")
-        
+    print("What is the text to count?")
+    str = ""
+    while True:
+        char = sys.stdin.read(1)
+        if char == "":
+            print("")
+            break
+        elif char == '\n':
+            str += char 
+            break 
+        str += char
+    return str    
 
+def print_info(str:str):
+    print("The text contains ", len(str), "characters:")
+    print(sum(1 for char in str if char.isupper()), "upper letters")
+    print(sum(1 for char in str if char.islower()), "lower letters")
+    print(sum(1 for char in str if char in string.punctuation), "punctuation marks")
+    print(sum(1 for char in str if char == ' ' or char == '\n'), "spaces")
+    print(sum(1 for char in str if char.isdigit()), "digits")
 
 def main():
     if len(argv) > 2:
@@ -13,8 +32,8 @@ def main():
         return 
 
     str = getstr()
-    print(str)
-    
+    print_info(str)    
+    return 
 
 if __name__ == "__main__":
     main()
