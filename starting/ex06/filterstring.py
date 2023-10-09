aaaa30d8a736cbfbef1498dbf6c944c7078b6b2d
gottie <gottiedev@gmail.com>
2023-10-09 16:23:34 +0200

    finish ex06

--- starting/ex06/filterstring.py
+++ starting/ex06/filterstring.py
@@ -1,3 +1,20 @@
-def filterstring(str: str, nbr : int) -> list:
-    pass
+from sys import argv
+from ft_filter import ft_filter
+    # creer un liste qui separe la string en mot -> utiliser une expression
+    # filter les string qui font moin de nbr char-> utiliser un lambda
 
+def main():
+    if len(argv) != 3:
+        print("AssertionError: the arguments are bad")
+        exit(1)
+    try:
+        nbr = int(argv[2])
+    except ValueError:
+        print("AssertionError: the arguments are bad")
+
+    word_list = argv[1].split()
+    print(list(ft_filter(lambda x: len(x) > nbr, word_list)))
+
+
+if __name__ == "__main__":
+    main()
--- starting/ex06/test_stringfilter.py
+++
@@ -1,7 +0,0 @@
-from filterstring import filterstring
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
